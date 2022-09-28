07164a054317cb1ae92cda90de55fe988eb8d648
olegustimchik <71694504+olegustimchik@users.noreply.github.com>
2022-09-28 22:09:24 +0300

    home work

--- hw2/Lab_2.java
+++ hw2/Lab_2.java
@@ -1,6 +1,51 @@
-package hw2;
+package hw2 ;
 
-import hw2.ThreadClass;
+class ThreadClass extends Thread{ 
+    ForFirstHalf first;
+    ForSecondHalf second;
+    
+    ThreadClass(ForFirstHalf first, ForSecondHalf second){ 
+        this.first = first ; 
+        this.second = second; 
+    }
+
+    @Override
+    public void run(){
+        synchronized (this){
+            System.out.println("first half thread");
+            int k1 = (int)Lab_2.randomNumber(10000, 20000); 
+            for(int i = 0; i < k1; i++){ 
+                this.first.setNumber(this.first.getNumber() + Lab_2.randomNumber(2, 5));
+                this.second.setSecondNumber(this.second.getSecondNumber() + Lab_2.randomNumber(2, 5));
+            }
+        } 
+
+    }
+}
+
+class ThreadClass2 extends Thread{ 
+    ForFirstHalf first;
+    ForSecondHalf second;
+    
+    ThreadClass2(ForFirstHalf first, ForSecondHalf second){ 
+        this.first = first ; 
+        this.second = second; 
+    }
+
+    @Override
+    public void run(){
+        synchronized (this){ 
+            System.out.println("second half thread");
+            int k1 = (int)Lab_2.randomNumber(10000, 20000); 
+            for(int i = 0; i < k1; i++){ 
+                this.first.setNumber(this.first.getNumber() + Lab_2.randomNumber(2, 5));
+                this.second.setSecondNumber(this.second.getSecondNumber() + Lab_2.randomNumber(2, 5));
+            }
+        }
+
+    }
+
+}
 
 class ForFirstHalf{ 
     private double number; 
