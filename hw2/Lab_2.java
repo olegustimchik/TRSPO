fbfb1287e0187acebd297383b1af78f3308272ce
olegustimchik <71694504+olegustimchik@users.noreply.github.com>
2022-09-29 13:40:11 +0300

    I changed my answer

--- hw2/Lab_2.java
+++ hw2/Lab_2.java
@@ -1,24 +1,37 @@
 package hw2 ;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.CountDownLatch;
+
 class ThreadClass extends Thread{ 
     ForFirstHalf first;
     ForSecondHalf second;
+    Semaphore mutex; 
+    CountDownLatch startAll; 
     
-    ThreadClass(ForFirstHalf first, ForSecondHalf second){ 
+    ThreadClass(ForFirstHalf first, ForSecondHalf second, CountDownLatch startAll){ 
         this.first = first ; 
         this.second = second; 
+        this.mutex = new Semaphore(1); 
+        this.startAll = startAll; 
     }
 
     @Override
     public void run(){
-        synchronized (this){
+        try{
+            this.mutex.acquire();
             System.out.println("first half thread");
             int k1 = (int)Lab_2.randomNumber(10000, 20000); 
             for(int i = 0; i < k1; i++){ 
                 this.first.setNumber(this.first.getNumber() + Lab_2.randomNumber(2, 5));
                 this.second.setSecondNumber(this.second.getSecondNumber() + Lab_2.randomNumber(2, 5));
             }
-        } 
+            this.startAll.countDown();
+            this.mutex.release();
+
+        }catch(InterruptedException e ){ 
+            System.out.println(   " error ");
+        }  
 
     }
 }
@@ -26,23 +39,32 @@ class ThreadClass extends Thread{
 class ThreadClass2 extends Thread{ 
     ForFirstHalf first;
     ForSecondHalf second;
+    Semaphore mutex;
+    CountDownLatch startAll; 
     
-    ThreadClass2(ForFirstHalf first, ForSecondHalf second){ 
+    ThreadClass2(ForFirstHalf first, ForSecondHalf second, CountDownLatch startAll){ 
         this.first = first ; 
         this.second = second; 
+        this.mutex = new Semaphore(1);
+        this.startAll = startAll; 
     }
 
     @Override
     public void run(){
-        synchronized (this){ 
+        try{
+            this.mutex.acquire();
             System.out.println("second half thread");
             int k1 = (int)Lab_2.randomNumber(10000, 20000); 
             for(int i = 0; i < k1; i++){ 
                 this.first.setNumber(this.first.getNumber() + Lab_2.randomNumber(2, 5));
                 this.second.setSecondNumber(this.second.getSecondNumber() + Lab_2.randomNumber(2, 5));
             }
-        }
+            this.startAll.countDown();
+            this.mutex.release();
 
+        }catch(InterruptedException e ){ 
+            System.out.println(" error ");
+        } 
     }
 
 }
@@ -84,11 +106,13 @@ class ForSecondHalf{
 }
 
 public class Lab_2{ 
-    public static void main(String[] args){ 
+    public static void main(String[] args) throws InterruptedException{ 
         // ForFirstHalf obj = new ForFirstHalf(2.3); 
         int randNum = (int)randomNumber(10, 20);
+        CountDownLatch startAll  = new CountDownLatch(randNum);
 
         int half = (int)randNum/2 ;
+        long start = System.nanoTime();
 
         Thread[] firstThreads = new ThreadClass[half];
         Thread[] secondThreads = new ThreadClass2[randNum - half];
@@ -98,26 +122,33 @@ public class Lab_2{
         // ForFirstHalf fhalf2 =  new ForFirstHalf();
         // ForSecondHalf shalf2 =  new ForSecondHalf();
 
-        firstThreads = initThreads_1(firstThreads, fhalf, shalf); 
-        secondThreads = initThreads_2(secondThreads, fhalf, shalf); 
+        firstThreads = initThreads_1(firstThreads, fhalf, shalf, startAll); 
+        secondThreads = initThreads_2(secondThreads, fhalf, shalf, startAll); 
+ 
 
         executeThreads(firstThreads);
         executeThreads(secondThreads);
 
+        startAll.await();
 
+        long end = System.nanoTime();
+        System.out.println(fhalf.getNumber());
+        System.out.println(shalf.getSecondNumber());
+        
+        System.out.println("\ntime: " + (end - start));
 
     }
 
-    public static Thread[] initThreads_1(Thread[] threads, ForFirstHalf first, ForSecondHalf second){ 
+    public static Thread[] initThreads_1(Thread[] threads, ForFirstHalf first, ForSecondHalf second, CountDownLatch startAll){ 
         for (int i = 0; i < threads.length; i++ ){ 
-            threads[i] = new ThreadClass(first, second);  
+            threads[i] = new ThreadClass(first, second, startAll);  
         }
         return threads; 
     }
 
-    public static Thread[] initThreads_2(Thread[] threads, ForFirstHalf first, ForSecondHalf second){ 
+    public static Thread[] initThreads_2(Thread[] threads, ForFirstHalf first, ForSecondHalf second, CountDownLatch startAll){ 
         for (int i = 0; i < threads.length; i++ ){ 
-            threads[i] = new ThreadClass2(first, second);  
+            threads[i] = new ThreadClass2(first, second, startAll);  
         }
         return threads; 
     }
@@ -127,6 +158,16 @@ public class Lab_2{
             threads[i].start();
         }
     }
+   
+    // public static void joinThreads(Thread[] threads){ 
+    //     for(int i = 0; i < threads.length; i++){ 
+    //         try{
+    //             threads[i].join();
+    //         } catch(InterruptedException e){ 
+    //             System.out.println("Error");
+    //         }
+    //     }
+    // }
     public static double randomNumber(int leastNumber, int upperBound){ 
         return Math.random()*(upperBound - leastNumber + 1 ) + leastNumber; 
     }
